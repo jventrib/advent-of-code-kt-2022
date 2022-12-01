f67bfa5787c534a96a59f3827effe45e267e4dc5
jventrib <jventrib@gmail.com>
2022-12-01 12:31:08 +0100

    day01 ok

--- src/commonMain/kotlin/Day01.kt
+++ src/commonMain/kotlin/Day01.kt
@@ -1,9 +1,14 @@
 val day01 = day<Int>(1) {
-    part1(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    part1(expectedExampleOutput = 24000, expectedOutput = 67450) {
+        caloriesList().max()
     }
 
-    part2(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    part2(expectedExampleOutput = 45000, expectedOutput = 199357) {
+        caloriesList().sortedDescending().take(3).sum()
     }
 }
+private fun Day<Int>.caloriesList(): List<Int> =
+    input.fold(listOf(0)) { acc, s ->
+        if (s.isNotEmpty()) acc.dropLast(1) + (acc.last() + s.toInt())
+        else acc + 0
+    }

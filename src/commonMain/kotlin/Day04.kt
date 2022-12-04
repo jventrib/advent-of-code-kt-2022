3ee02c9928eb03a0613d615554adf458ffa2f640
jventrib <jventrib@gmail.com>
2022-12-04 13:28:15 +0100

    Day04 OK

--- src/commonMain/kotlin/Day04.kt
+++ src/commonMain/kotlin/Day04.kt
@@ -1,9 +1,19 @@
 val day04 = day(4) {
-    part1(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    val pairList = input.map { line ->
+        val r1 = line.substringBefore(',').let { range ->
+            range.split('-').let { IntRange(it[0].toInt(), it[1].toInt()) }
+        }
+        val r2 = line.substringAfter(',').let { range ->
+            range.split('-').let { IntRange(it[0].toInt(), it[1].toInt()) }
+        }
+        Pair(r1, r2)
     }
 
-    part2(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    part1(expectedExampleOutput = 2, expectedOutput = 588) {
+        pairList.count { (r1, r2) -> r1.toList().containsAll(r2.toList()) || r2.toList().containsAll(r1.toList()) }
+    }
+
+    part2(expectedExampleOutput = 4, expectedOutput = 911) {
+        pairList.count { (r1, r2) -> r1.intersect(r2).isNotEmpty() || r2.intersect(r1).isNotEmpty() }
     }
 }

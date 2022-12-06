d26942fbeee1ba2b2baf83bc7b2c72494afebebb
jventrib <jventrib@gmail.com>
2022-12-06 13:30:08 +0100

    Day06 OK

--- src/commonMain/kotlin/Day06.kt
+++ src/commonMain/kotlin/Day06.kt
@@ -1,9 +1,12 @@
-val day06 = day<Long>(6) {
-    part1(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+val day06 = day(6) {
+    part1(expectedExampleOutput = 7, expectedOutput = 1651) {
+        doPart(4)
     }
 
-    part2(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    part2(expectedExampleOutput = 19, expectedOutput = 3837) {
+        doPart(14)
     }
 }
+
+private fun Day<Int>.doPart(size: Int) = input.first().asSequence().windowed(size)
+    .indexOfFirst { chars -> chars.groupingBy { it }.eachCount().values.max() < 2 } + size

ee174ad9ca2c07b1653aa447be78680b4a792396
jventrib <jventrib@gmail.com>
2022-12-07 21:09:35 +0100

    Day07 OK

--- src/commonMain/kotlin/Day07.kt
+++ src/commonMain/kotlin/Day07.kt
@@ -1,79 +1,50 @@
+import Node.*
+
 val day07 = day(7) {
-    part1(expectedExampleOutput = 95437, expectedOutput = 0) {
-
-        val fold = input.fold(Node.Dir("/", setOf(), null)) { node: Node.Dir, l ->
-            when {
-                l.startsWith('$') -> {
-                    when {
-                        l.startsWith("$ cd") -> {
-                            when (val newDir = l.substringAfter("$ cd")) {
-                                "/" -> {
-                                    var root = node
-                                    while (node.parent != null) {
-                                        root = root.parent!!
-                                    }
-                                    root
-                                }
-                                ".." -> node.parent!!
-                                else -> Node.Dir(newDir, setOf(), node.parent)
-                            }
-                        }
-                        l == "$ ls" -> node
-                        else -> throw Exception("Unrecognized line")
-                    }
-                }
-
-                l.startsWith("dir") -> {
-                    node
-                    Node.Dir(node.name, node.children+ Node.Dir(), )
-
-                }
-
-                else -> {
-                    node
-                }
-            }
-
-        }
-        fold
+    part1(expectedExampleOutput = 95437, expectedOutput = 1297683) {
+        dirs().map { it.getNodeSize() }.filter { it < 100000 }.sum()
     }
 
-    part2(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    part2(expectedExampleOutput = 24933642, expectedOutput = 5756764) {
+        val dirs = dirs()
+        val freeSize = 70000000 - dirs.first().getNodeSize()
+        dirs.drop(1).filter { freeSize + it.getNodeSize() > 30000000 }.minOf { it.getNodeSize() }
+
     }
 }
 
-private sealed class Node(val name: String, val parent: Dir?) {
-    abstract fun getSize(): Int
+private fun Day<Int>.dirs(): List<Dir> {
+    var node = Dir("/", mutableSetOf(), null)
+    input.drop(1).forEach { l ->
+        when {
+            l.startsWith("$ cd ..") -> node.parent?.also { node = it }
+            l.startsWith("$ cd ") -> node =
+                node.children.filterIsInstance<Dir>().first { it.name == l.substringAfter("$ cd ") }
 
-    class Dir(name: String, val children: Set<Node>, parent: Dir?) : Node(name, parent) {
-        override fun getSize(): Int = children.sumOf { getSize() }
+            l.startsWith("dir") -> node.children.add(Dir(l.substringAfter("dir "), mutableSetOf(), node))
+            l.substringBefore(' ').isNumeric() -> node.children.add(
+                l.split(' ').let { File(it[1], it[0].toInt(), node) })
+        }
+    }
+    while (node.parent != null) {
+        node = node.parent!!
+    }
+    return node.flattenDir()
+}
+
+private sealed class Node(val name: String, val parent: Dir?) {
+    abstract fun getNodeSize(): Int
+
+    class Dir(name: String, val children: MutableSet<Node>, parent: Dir?) : Node(name, parent) {
+        override fun getNodeSize(): Int = children.sumOf { it.getNodeSize() }
+
+        fun flattenDir(): List<Dir> {
+            val filterIsInstance = this.children.filterIsInstance<Dir>()
+            return listOf(this) + filterIsInstance.flatMap { it.flattenDir() }
+        }
     }
 
     class File(name: String, val size: Int, parent: Dir?) : Node(name, parent) {
-        override fun getSize(): Int = size
+        override fun getNodeSize(): Int = size
     }
-
-    override fun toString(): String {
-        return "Node(name='$name', size='${getSize()}')"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null || this::class != other::class) return false
-
-        other as Node
-
-        if (name != other.name) return false
-        if (parent != other.parent) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = name.hashCode()
-        result = 31 * result + (parent?.hashCode() ?: 0)
-        return result
-    }
-
 }

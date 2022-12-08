3a4e2fab25579557febd8f3b1861f5a5c03f52dd
jventrib <jventrib@gmail.com>
2022-12-08 22:27:48 +0100

    Day08 OK

--- src/commonMain/kotlin/Day08.kt
+++ src/commonMain/kotlin/Day08.kt
@@ -1,32 +1,31 @@
 val day08 = day(8) {
-    part1(expectedExampleOutput = 0, expectedOutput = 0) {
-        val fromLeft = input.drop(1).dropLast(1).map { row ->
-            row.dropLast(1).foldIndexed(listOf<Pair<Int, Int>>()) { i, acc, c ->
-                val digitToInt = c.digitToInt()
-                if (acc.isEmpty() || digitToInt > acc.maxOf { it.first }) {
-                    acc + (digitToInt to i)
-                } else {
-                    acc
-                }
-            }.drop(1)
-        }
-        val fromRight = input.drop(1).dropLast(1).map { row ->
-            row.drop(1).foldRightIndexed(listOf<Pair<Int, Int>>()) { i, c, acc ->
-                val digitToInt = c.digitToInt()
-                if (acc.isEmpty() || digitToInt > acc.maxOf { it.first }) {
-                    acc + (digitToInt to i+1)
-                } else {
-                    acc
-                }
-            }.drop(1)
-        }
-        
-        
-        
-        0
+    val points = input.mapIndexed { y, row -> row.mapIndexed { x, c -> Point(x, y, c.digitToInt()) } }
+    val transposedPoints = points.flatten().groupBy { it.x }.values.toList()
+    part1(expectedExampleOutput = 21, expectedOutput = 1814) {
+        val fromLeft = getFromLeft(points)
+        val fromRight = getFromRight(points)
+        val fromTop = getFromLeft(transposedPoints)
+        val fromBottom = getFromRight(transposedPoints)
+        (fromLeft + fromRight + fromTop + fromBottom).distinctBy { it }.count()
     }
 
-    part2(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    part2(expectedExampleOutput = 8, expectedOutput = 330786) {
+        val width = points.maxOf { it.size }
+        val height = points.size
+        points.flatten().maxOfOrNull { p ->
+            val left = (p.x - 1 downTo 0).takeWhileInclusive { points[p.y][it].value < p.value }.count()
+            val right = (p.x + 1 until width).takeWhileInclusive { points[p.y][it].value < p.value }.count()
+            val top = (p.y - 1 downTo 0).takeWhileInclusive { points[it][p.x].value < p.value }.count()
+            val bottom = (p.y + 1 until height).takeWhileInclusive { points[it][p.x].value < p.value }.count()
+            right * left * top * bottom
+        }
     }
 }
+
+private fun getFromLeft(m: List<List<Point>>) =
+    m.map { row -> row.fold(listOf<Point>()) { acc, c -> if (acc.isEmpty() || c.value > acc.maxOf { it.value }) acc + c else acc } }
+        .flatten()
+
+private fun getFromRight(m: List<List<Point>>) =
+    m.map { row -> row.foldRight(listOf<Point>()) { c, acc -> if (acc.isEmpty() || c.value > acc.maxOf { it.value }) acc + c else acc } }
+        .flatten()

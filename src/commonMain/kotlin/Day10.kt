4a13ba64ce2a2c98aeae63ba7207a157015b0f24
jventrib <jventrib@gmail.com>
2022-12-10 16:54:02 +0100

    Day10 part2

--- src/commonMain/kotlin/Day10.kt
+++ src/commonMain/kotlin/Day10.kt
@@ -1,9 +1,9 @@
 val day10 = day(10) {
     part1(expectedExampleOutput = 13140, expectedOutput = 13820) {
-        val cycles = input.fold(listOf<Int>()) { acc, s ->
+        val cycles = input.fold(listOf(1)) { acc, s ->
             when (s) {
-                "noop" -> acc + acc.lastOr1()
-                else -> acc + acc.lastOr1() + (acc.lastOr1() + s.substringAfter("addx ").toInt())
+                "noop" -> acc + acc.last()
+                else -> acc + acc.last() + (acc.last() + s.substringAfter("addx ").toInt())
             }
         }
         cycles.run {
@@ -12,10 +12,23 @@ val day10 = day(10) {
     }
 
     part2(expectedExampleOutput = 0, expectedOutput = 0) {
+        val cycles = input.fold(listOf(1)) { acc, s ->
+            when (s) {
+                "noop" -> acc + acc.last()
+                else -> acc + acc.last() + (acc.last() + s.substringAfter("addx ").toInt())
+            }
+        }
+        cycles.forEachIndexed { i, s ->
+            val sprite = s.let { it - 1..it + 1 }
+            when {
+                sprite.contains(i % 40) -> print('#')
+                else -> print('.')
+            }
+            if ((i + 1) % 40 == 0) println()
+        }
+        println()
         0
     }
 }
 
-private fun List<Int>.lastOr1() = lastOrNull() ?: 1
-
-private fun List<Int>.ss(cycle: Int) = this[cycle - 2] * cycle
+private fun List<Int>.ss(cycle: Int) = this[cycle - 1] * cycle
